b828ce2a9c82abc6ccc7ebf67f4f81dd2c396c3a
Minjae Jung <noahjung87@gmail.com>
2021-03-11 00:16:40 +0000

    pair work done

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,23 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present and vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'I'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'G'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'H'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'T'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'H'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'O'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'U'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'S'],
+    ], 'LIGHTHOUS')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present and horizontal", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +50,27 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("If word matrix is empty, should return false", function() {
+    const result = wordSearch([], 'SEINFELD')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if no word supplied", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], "")
+
+    assert.isFalse(result);
   });
 });
